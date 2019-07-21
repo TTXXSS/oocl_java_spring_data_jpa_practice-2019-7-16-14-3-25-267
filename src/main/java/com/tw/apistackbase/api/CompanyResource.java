f7adb9a6cc9b165e14b998f523172ed0b81888f8
TTXXSS <1374230455@qq.com>
2019-07-21 15:21:04 +0800

    feat:finish class company

--- src/main/java/com/tw/apistackbase/api/CompanyResource.java
+++ src/main/java/com/tw/apistackbase/api/CompanyResource.java
@@ -1,19 +1,32 @@
 package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.core.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyResource {
-    
+    @Autowired
+    private CompanyRepository companyRepository;
     @GetMapping(produces = {"application/json"})
     public Iterable<Company> list() {
-        return null;
+
+        return companyRepository.findAll();
     }
     
-    @PostMapping(produces = {"application/json"})
+    @PostMapping()
     public Company add(@RequestBody Company company) {
-        return null;
+        return companyRepository.save(company);
+    }
+    @PutMapping
+    public Company update(@RequestBody Company company){
+        return companyRepository.save(company);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        companyRepository.deleteById(id);
     }
 }
